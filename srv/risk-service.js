5ed36b820086c0f4f84f7ef55b72dc3c3fe668e8
d038359 <d038359@exchange.sap.corp>
2021-09-20 11:08:14 +0000

    Update Mon 20 Sep 2021 11:08:14 AM UTC

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -6,10 +6,7 @@ const cds = require('@sap/cds')
  */
 module.exports = cds.service.impl(async function() {
 
-    const bupaService = await cds.connect.to('API_BUSINESS_PARTNER');
-    const bupa = {
-      run: query => bupaService.send({ query, headers: process.env.SANDBOX_API_KEY ? { APIKey: process.env.SANDBOX_API_KEY } : {} })
-    }
+    const bupa = await cds.connect.to('API_BUSINESS_PARTNER');
 
     this.on('READ', 'Suppliers', async req => {
         return bupa.run(req.query);
